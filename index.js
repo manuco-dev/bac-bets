25c534294e9bd0c4f46d298cceca70853d2c6b23
alexkof158 <63129192+alexkof158@users.noreply.github.com>
2024-11-20 14:21:25 -0500

    Cambios Realizados

--- index.js
+++ index.js
@@ -1,40 +1,36 @@
 const { dbConnection } = require('./db/config');
-
 const express = require('express');
+const cors = require('cors');
 
-//Cors permite que mi cliente se conecte desde otro servidor
-
-const cors = require('cors')
-
-dbConnection()
-require('dotenv').config()
+dbConnection();
+require('dotenv').config();
 
 const port = process.env.PORT || 3609;
 
-// Crear Server
-
+// Crear servidor
 const app = express();
 
-app.use(express.json())
-app.use(cors())
+// Configurar CORS para permitir solo tu dominio
+const corsOptions = {
+  origin: 'https://stats-nba.netlify.app', // Especifica tu dominio
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+  allowedHeaders: ['Content-Type', 'Authorization'], // Encabezados permitidos
+};
 
+// Middleware de CORS
+app.use(cors(corsOptions));
 
-// Ruta Pulic
-app.use(express.static('public'))
+// Middleware de JSON
+app.use(express.json());
 
-app.use('/api/midas', require('./routes/midas'))
-app.use('/api/mlb', require('./routes/mlb'))
-app.use('/api/uefa', require('./routes/uefa'))
-app.use('/api/uefa', require('./routes/uefa'))
-app.use('/api/prediccions', require('./routes/prediccions'))
+// Rutas de la API
+app.use('/api/midas', require('./routes/midas'));
+app.use('/api/mlb', require('./routes/mlb'));
+app.use('/api/uefa', require('./routes/uefa'));
+app.use('/api/prediccions', require('./routes/prediccions'));
+app.use('/api/usuarios', require('./routes/usuarios'));
 
-
-
-//Usuarios
-
-app.use('/api/usuarios', require('./routes/usuarios'))
-
-//Escuchar las peticiones
-app.listen( port, ()=>{
-    console.log(`Servidor corriendo en el puerto ${port}`)
-} )
+// Escuchar las peticiones
+app.listen(port, () => {
+    console.log(`Servidor corriendo en el puerto ${port}`);
+});
